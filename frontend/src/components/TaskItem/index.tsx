2cc10eceb3b61b5807b0a016ced730d6101172c3
Gabriel Nogueira <gabrieljcf@hotmail.com>
2020-09-24 00:39:34 -0300

    adding feature of create task

--- frontend/src/components/TaskItem/index.tsx
+++ frontend/src/components/TaskItem/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
+import './styles.css';
+
 interface TaskProps {
   id: string;
   name: string;
   status: number;
 }
 
+
 const TaskItem: React.FC<TaskProps> = ({ name, id }) => {
   return (
-    <div className="container-item">
       <article className="card">
         <p className="project-name">{name}</p>
+        
+        <div className="container-buttons-tasks">
+          <button className="update-button">Editar</button>
+          <button className="delete-button">Excluir</button>
+          <button className="complete-button">Completar</button>
+        </div>
       </article>
-    </div>
   );
 }
 
--- frontend/src/pages/Tasks/index.tsx
+++ frontend/src/pages/Tasks/index.tsx
@@ -5,6 +5,10 @@ import TaskItem from '../../components/TaskItem';
 
 import api from '../../services/api';
 
+import arrowIcon from '../../assets/images/icons/arrow-right.svg';
+
+import './styles.css';
+
 interface TaskProps {
   match: {
     url: string;
@@ -15,33 +19,74 @@ interface Task {
   id: string;
   name: string;
   status: number;
-}
+};
 
 function ListTasks(props: TaskProps) {
+
   const [tasks, setTasks] = useState([]);
+  const [taskName, setTaskName] = useState('');
+
   const route = props.match.url;
 
   useEffect(() => {
+
     api.get(route)
       .then(response => {
         const results = response.data;
         setTasks(results);
       });
+
   }, [route]);
 
-  return (
-    <div>
-      <PageHeader />
-      
-      {tasks.map(({id, name, status}: Task) => (
-        <TaskItem 
-          key={id} 
-          id={id} 
-          name={name}
-          status={status} />)
-      )}
+  async function handleAddTask() {
+    if (!taskName) {
+      alert('Informe uma tarefa');
+      return;
+    }
 
-    </div>
+    try {
+
+      const response = await api.post(route, { name: taskName });
+      const newTask = [...tasks, response.data] as [];
+
+      setTasks(newTask);
+      setTaskName('');
+
+    } catch (error) {
+      alert('Erro ao cadastrar task tente novamente! :(');
+    }
+  }
+
+  return (
+    <>
+      <PageHeader />
+      <div className="container-item">
+
+        <div className="container-input">
+          <input
+            type="text"
+            name="task"
+            id="task"
+            placeholder="Qual tarefa faremos hoje?"
+            onChange={event => setTaskName(event.target.value)}
+            value={taskName}
+          />
+          <button onClick={handleAddTask}>
+            <img src={arrowIcon} alt="Salvar" />
+          </button>
+
+        </div>
+
+        {tasks.map(({ id, name, status }: Task) => (
+          <TaskItem
+            key={id}
+            id={id}
+            name={name}
+            status={status} />)
+        )}
+
+      </div>
+    </>
   );
 }
 
